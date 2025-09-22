cd617ef6b9b0bf7de717718fc19ce154f5908e9a
oenmckinley <oenmckinley@gmail.com>
2025-09-22 15:08:18 -0500

    Update Conversation.tsx

--- src/public/text-misalignment/assets/Conversation.tsx
+++ src/public/text-misalignment/assets/Conversation.tsx
@@ -1,9 +1,6 @@
 import * as d3 from 'd3';
 import { useEffect, useState } from 'react';
 import { useChartDimensions } from './hooks/useChartDimensions';
-import { Bars } from './chartcomponents/Bars';
-import { NumericAxisV } from './chartcomponents/NumericAxisV';
-import { OrdinalAxisHWithDotMarks } from './chartcomponents/OrdinalAxisHWithDotMarks';
 
 const chartSettings = {
   marginBottom: 40,
@@ -16,7 +13,6 @@ const chartSettings = {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function Conversation({ parameters }: { parameters: any }) {
-  const tickLength = 6;
   const [ref, dms] = useChartDimensions(chartSettings);
 
   const [timeUp, setTimeUp] = useState(false);
@@ -29,25 +25,7 @@ function Conversation({ parameters }: { parameters: any }) {
     return () => clearTimeout(timer);
   }, []);
 
-  const xScale = d3
-    .scaleBand()
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    .domain(parameters.data.map((d: { name: any }) => d.name))
-    .range([0, dms.boundedWidth])
-    .padding(0.2);
 
-  const yScale = d3
-    .scaleLinear()
-    .domain([100, 0])
-    .range([0, dms.boundedHeight]);
-
-  const stimulus = require('logo.png');
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const yAxisTickFilter = (ticks: any[]) => ticks.filter((t, i) => i === 0 || i === ticks.length - 1);
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const xAxisTickFilter = (ticks: any[]) => ticks.filter((t, i) => parameters.selectedIndices.includes(i));
 
   return (
     <div className="Chart__wrapper" ref={ref} style={{ height: 400 }}>
@@ -56,7 +34,7 @@ function Conversation({ parameters }: { parameters: any }) {
           <h2>Time is up! Please click Next.</h2>
         </div>
       ) : (
-        <img alt='stimulus' style={{ width: 100 }} src={String(stimulus)} />
+        <img alt='stimulus' style={{ width: 100 }} src={'./logo.png'} />
       )}
     </div>
   );
